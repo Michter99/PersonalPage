387667b0a13e6107c3b8592dbeccffdd24f0e534
Miguel Richter <63754580+Michter99@users.noreply.github.com>
2024-01-06 19:53:27 -0600

    New Design 2024

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
-    window.location.href = `mailto:${"miguelrichter99@hotmail.com"}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+document.addEventListener("DOMContentLoaded", function () {
+  var navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+  var navbarToggler = document.querySelector(".navbar-toggler");
+
+  navLinks.forEach(function (navLink) {
+    navLink.addEventListener("click", function () {
+      if (!navbarToggler.classList.contains("collapsed")) {
+        navbarToggler.click();
+      }
+    });
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var navbarNavLinks = document.querySelectorAll('.navbar-nav>li>a');
-    navbarNavLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            document.querySelector('.navbar-collapse').classList.remove('show');
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggleButton = document.getElementById("theme-toggle");
+
+  themeToggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    themeToggleButton.textContent = document.body.classList.contains(
+      "dark-mode"
+    )
+      ? "LIGHT MODE"
+      : "DARK MODE";
+
+    const navbar = document.getElementsByClassName("navbar")[0];
+    if (document.body.classList.contains("dark-mode")) {
+      navbar.classList.remove("navbar-light", "bg-light");
+      navbar.classList.add("navbar-dark", "bg-dark");
+    } else {
+      navbar.classList.remove("navbar-dark", "bg-dark");
+      navbar.classList.add("navbar-light", "bg-light");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.add("transparent");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.remove("transparent");
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.add("transparent");
+      navbar.classList.remove("scrolled");
+    }
+  });
 });
